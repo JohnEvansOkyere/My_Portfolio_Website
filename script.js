26685e6981e5d70101227cf1f944cc6f402070ac
JohnEvansOkyere <148287702+JohnEvansOkyere@users.noreply.github.com>
2024-09-11 08:10:35 +0000

    Website updates

--- script.js
+++ script.js
@@ -27,21 +27,6 @@ function closemenu(){
 }
 
 
+/***********************CONTACT FORM******************* */
 
-document.addEventListener('DOMContentLoaded', function() {
-    var contactForm = document.getElementById('contactForm');
-    var successMessage = document.getElementById('successMessage');
 
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-
-        // Simulate form submission (replace this with your actual form submission code)
-        setTimeout(function() {
-            // Show the success message
-            successMessage.style.display = 'block';
-
-            // Optionally, clear the form fields
-            contactForm.reset();
-        }, 500); // Simulating a delay for form submission
-    });
-});
